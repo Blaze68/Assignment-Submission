fe9b2421c85ab79db463a4ae963d1168428bb280
Blaze68 <37133284+Blaze68@users.noreply.github.com>
2021-07-16 14:56:54 +0530

    Day 1 Assignment Submitted

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const Apple=()=> {
+    return(
+        <div>
+            <h1 > Hello </h1>
+            <h2> ssup all </h2>
+            <Button buttonText="RED"/>
+            <Button buttonText="BLUE"/>
+            <Button buttonText="ORANGE"/>
+        </div>
+    )
+}
+
+export default Apple
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Apple from "./app"
+
+
+
+
+ReactDOM.render(<Apple />,document.getElementById("root"))
